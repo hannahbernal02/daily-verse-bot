90c3d6e111c3070d11ab5dfcfccb330a6aed7417
Hannah Bernal <hannahbernal02@gmail.com>
2020-08-26 23:33:23 -0700

    Fixed minor spelling error

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ def create_api():
 
     consumer_key = environ['CONSUMER_KEY']
     consumer_secret = environ['CONSUMER_SECRET']
-    access_token = environ['ACEESS_TOKEN']
+    access_token = environ['ACCESS_TOKEN']
     access_token_secret = environ["ACCESS_TOKEN_SECRET"]
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
